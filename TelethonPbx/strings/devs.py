0e535dfef3c244ca982c5327d2b8df994b4db4a2
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-02-08 21:46:33 +0530

    Add files via upload

---
+++ TelethonPbx/strings/devs.py
@@ -0,0 +1,26 @@
+DEVLIST = [
+    "6566179661",   # bad munda
+    "6898413162", # bad munda 2
+    "6415940074", # dvil 
+]
+
+HARMFUL = [
+    "base64",
+    "bash",
+    "get_me()",
+    "phone",
+    "os.system",
+    "sys.stdout",
+    "sys.stderr",
+    "subprocess",
+    "session.save()",
+    "PBXBOT_SESSION",
+    "INSTAGRAM_SESSION",
+    "DATABASE_URL",
+    "BOT_TOKEN",
+    "API_HASH",
+    "APP_ID",
+    "HEROKU_API_KEY",
+]
+
+INVALID_UPLOAD = ["PB-XBot.session-journal", "PB-XBot.session", "insta/settings.json"]

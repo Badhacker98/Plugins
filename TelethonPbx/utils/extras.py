e5712413d5366f87f734783109a5efa98a4d449b
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-02-08 21:47:09 +0530

    Add files via upload

---
+++ TelethonPbx/utils/extras.py
@@ -0,0 +1,102 @@
+import asyncio
+import os
+import re
+
+from PbxConfig import Config
+from TelethonPbx.helpers.pasters import pasty
+
+
+# either edit or reply that msg
+async def edit_or_reply(
+    event,
+    text,
+    parse_mode=None,
+    link_preview=None,
+    file_name=None,
+    aslink=False,
+    deflink=False,
+    noformat=False,
+    linktext=None,
+    caption=None,
+):
+    link_preview = link_preview or False
+    reply_to = await event.get_reply_message()
+    if len(text) < 4096 and not deflink:
+        parse_mode = parse_mode or "md"
+        if event.sender_id in Config.SUDO_USERS:
+            if reply_to:
+                return await reply_to.reply(
+                    text, parse_mode=parse_mode, link_preview=link_preview
+                )
+            return await event.reply(
+                text, parse_mode=parse_mode, link_preview=link_preview
+            )
+        await event.edit(text, parse_mode=parse_mode, link_preview=link_preview)
+        return event
+    if not noformat:
+        asciich = ["**", "`", "__"]
+        for i in asciich:
+            text = re.sub(rf"\{i}", "", text)
+    if aslink or deflink:
+        linktext = linktext or "Message was to big so pasted to bin"
+        response = await pasty(event, text)
+        text = linktext + f"[BIN]({response['url']}) •• [RAW]({response['raw']})"
+        if event.sender_id in Config.SUDO_USERS:
+            if reply_to:
+                return await reply_to.reply(text, link_preview=link_preview)
+            return await event.reply(text, link_preview=link_preview)
+        await event.edit(text, link_preview=link_preview)
+        return event
+    file_name = file_name or "output.txt"
+    caption = caption or None
+    with open(file_name, "w+") as output:
+        output.write(text)
+    if reply_to:
+        await reply_to.reply(caption, file=file_name)
+        await event.delete()
+        return os.remove(file_name)
+    if event.sender_id in Config.SUDO_USERS:
+        await event.reply(caption, file=file_name)
+        await event.delete()
+        return os.remove(file_name)
+    await event.client.send_file(event.chat_id, file_name, caption=caption)
+    await event.delete()
+    os.remove(file_name)
+
+
+# delete timeout
+async def delete_Pbx(event, text, time=None, parse_mode=None, link_preview=None):
+    parse_mode = parse_mode or "md"
+    link_preview = link_preview or False
+    time = time or 10
+    if event.sender_id in Config.SUDO_USERS:
+        reply_to = await event.get_reply_message()
+        Pbxevent = (
+            await reply_to.reply(text, link_preview=link_preview, parse_mode=parse_mode)
+            if reply_to
+            else await event.reply(
+                text, link_preview=link_preview, parse_mode=parse_mode
+            )
+        )
+    else:
+        Pbxevent = await event.edit(
+            text, link_preview=link_preview, parse_mode=parse_mode
+        )
+    await asyncio.sleep(time)
+    return await Pbxevent.delete()
+
+
+async def parse_error(event, error, auto_parse=True, delete=True, time=10):
+    if delete:
+        if auto_parse:
+            await delete_Pbx(event, f"**ERROR !!** \n\n`{error}`", time)
+        else:
+            await delete_Pbx(event, f"**ERROR !!** \n\n{error}", time)
+    else:
+        if auto_parse:
+            await edit_or_reply(event, f"**ERROR !!** \n\n`{error}`")
+        else:
+            await edit_or_reply(event, f"**ERROR !!** \n\n{error}")
+
+
+# Pbxbot
---
+++ TelethonPbx/utils/plug.py
@@ -0,0 +1,115 @@
+import importlib
+import logging
+import os
+import sys
+from pathlib import Path
+
+from PbxConfig import Config
+from telethon.tl.types import InputMessagesFilterDocument
+from TelethonPbx.clients.client_list import client_id
+from TelethonPbx.clients.decs import Pbx_cmd
+from TelethonPbx.clients.logger import LOGGER as LOGS
+from TelethonPbx.clients.session import H2, H3, H4, H5, Pbx, PbxBot
+from TelethonPbx.utils.cmds import CmdHelp
+from TelethonPbx.utils.decorators import admin_cmd, command, sudo_cmd
+from TelethonPbx.utils.extras import delete_Pbx, edit_or_reply
+from TelethonPbx.utils.globals import LOAD_PLUG
+
+
+# load plugins
+def load_module(shortname):
+    if shortname.startswith("__"):
+        pass
+    elif shortname.endswith("_"):
+        import TelethonPbx.utils
+
+        path = Path(f"TelethonPbx/plugins/{shortname}.py")
+        name = "TelethonPbx.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        LOGS.info("PbxBot - Successfully imported " + shortname)
+    else:
+        import TelethonPbx.utils
+
+        path = Path(f"TelethonPbx/plugins/{shortname}.py")
+        name = "TelethonPbx.plugins.{}".format(shortname)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.bot = Pbx
+        mod.H1 = Pbx
+        mod.H2 = H2
+        mod.H3 = H3
+        mod.H4 = H4
+        mod.H5 = H5
+        mod.Pbx = Pbx
+        mod.PbxBot = PbxBot
+        mod.tbot = PbxBot
+        mod.tgbot = Pbx.tgbot
+        mod.command = command
+        mod.CmdHelp = CmdHelp
+        mod.client_id = client_id
+        mod.logger = logging.getLogger(shortname)
+        mod.Config = Config
+        mod.borg = Pbx
+        mod.Pbxbot = Pbx
+        mod.edit_or_reply = edit_or_reply
+        mod.eor = edit_or_reply
+        mod.delete_Pbx = delete_Pbx
+        mod.eod = delete_Pbx
+        mod.Var = Config
+        mod.admin_cmd = admin_cmd
+        mod.Pbx_cmd = Pbx_cmd
+        mod.sudo_cmd = sudo_cmd
+        sys.modules["userbot.utils"] = TelethonPbx
+        sys.modules["userbot"] = TelethonPbx
+        sys.modules["userbot.events"] = TelethonPbx
+        spec.loader.exec_module(mod)
+        # for imports
+        sys.modules["TelethonPbx.plugins." + shortname] = mod
+        LOGS.info("🙈 ᴘʙxʙᴏᴛ 🙈 - Successfully Imported " + shortname)
+
+
+# remove plugins
+def remove_plugin(shortname):
+    try:
+        try:
+            for i in LOAD_PLUG[shortname]:
+                Pbx.remove_event_handler(i)
+            del LOAD_PLUG[shortname]
+
+        except BaseException:
+            name = f"TelethonPbx.plugins.{shortname}"
+
+            for i in reversed(range(len(Pbx._event_builders))):
+                ev, cb = Pbx._event_builders[i]
+                if cb.__module__ == name:
+                    del Pbx._event_builders[i]
+    except BaseException:
+        raise ValueError
+
+
+async def plug_channel(client, channel):
+    if channel != 0:
+        LOGS.info("👻 ᴘʙxʙᴏᴛ 👻 - PLUGIN CHANNEL DETECTED.")
+        LOGS.info("😈 ᴘʙxʙɪᴛ 😈 - Starting to load extra plugins.")
+        plugs = await client.get_messages(channel, None, filter=InputMessagesFilterDocument)
+        total = int(plugs.total)
+        for plugins in range(total):
+            plug_id = plugs[plugins].id
+            plug_name = plugs[plugins].file.name
+            if os.path.exists(f"TelethonPbx/plugins/{plug_name}"):
+                return
+            downloaded_file_name = await client.download_media(
+                await client.get_messages(channel, ids=plug_id),
+                "TelethonPbx/plugins/",
+            )
+            path1 = Path(downloaded_file_name)
+            shortname = path1.stem
+            try:
+                load_module(shortname.replace(".py", ""))
+            except Exception as e:
+                LOGS.error(str(e))
+
+
+# Pbxbot

1b815a39eab2273f6b9f93c62a3ad6c52f7a8245
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-02-08 21:45:40 +0530

    Create __init__.py

---
+++ TelethonPbx/strings/__init__.py
@@ -0,0 +1,6 @@
+from TelethonPbx.strings.devs import *
+from TelethonPbx.strings.fun_str import *
+from TelethonPbx.strings.meme_str import *
+from TelethonPbx.strings.others import *
+from TelethonPbx.strings.quotes import *
+from TelethonPbx.strings.rands import *

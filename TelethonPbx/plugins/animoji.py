f4c686a13260ead06d33da0ea7e92ca5fa23320c
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-02-11 14:07:35 +0530

    Add files via upload

---
+++ TelethonPbx/plugins/animoji.py
@@ -0,0 +1,466 @@
+import asyncio
+from collections import deque
+
+from TelethonPbx.plugins import *
+
+
+@Pbx_cmd(pattern="think$")
+async def _(event):
+    event = await eor(event, "think")
+    deq = deque(list("🤔🧐🤔🧐🤔🧐"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@Pbx_cmd(pattern="ccry$")
+async def cry(e):
+    await eor(e, "(;´༎ຶД༎ຶ)")
+
+
+@Pbx_cmd(pattern="fap$")
+async def _(event):
+    event = await eor(event, "fapping(°_°)")
+    deq = deque(list("🍆✊🏻💦"))
+    for _ in range(32):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@Pbx_cmd(pattern="lmao$")
+async def _(event):
+    event = await eor(event, "lmao")
+    deq = deque(list("😂🤣😂🤣😂🤣"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@Pbx_cmd(pattern="nothappy$")
+async def _(event):
+    event = await eor(event, "nathappy")
+    deq = deque(list("😁☹️😁☹️😁☹️😁"))
+    for _ in range(48):
+        await asyncio.sleep(0.4)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@Pbx_cmd(pattern="clock$")
+async def _(event):
+    event = await eor(event, "clock")
+    deq = deque(list("🕙🕘🕗🕖🕕🕔🕓🕒🕑🕐🕛"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@Pbx_cmd(pattern="muah$")
+async def _(event):
+    event = await eor(event, "muah")
+    deq = deque(list("😗😙😚😚😘"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@Pbx_cmd(pattern="heart$")
+async def _(event):
+    event = await eor(event, "heart")
+    deq = deque(list("❤️🧡💛💚💙💜🖤"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@Pbx_cmd(pattern="gym$")
+async def _(event):
+    event = await eor(event, "gym")
+    deq = deque(list("🏃‍🏋‍🤸‍🏃‍🏋‍🤸‍🏃‍🏋‍🤸‍"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@Pbx_cmd(pattern="earth$")
+async def _(event):
+    event = await eor(event, "earth")
+    deq = deque(list("🌏🌍🌎🌎🌍🌏🌍🌎"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@Pbx_cmd(pattern="moon$")
+async def _(event):
+    event = await eor(event, "moon")
+    deq = deque(list("🌗🌘🌑🌒🌓🌔🌕🌖"))
+    for _ in range(48):
+        await asyncio.sleep(0.1)
+        await event.edit("".join(deq))
+        deq.rotate(1)
+
+
+@Pbx_cmd(pattern="lovestory$")
+async def _(event):
+    animation_interval = 3
+    animation_ttl = range(0, 103)
+    await eor(event, "Let me tel you")
+    animation_chars = [
+        "1 ❤️ love story",
+        "  😐             😕 \n/👕\         <👗\ \n 👖               /|",
+        "  😉          😳 \n/👕\       /👗\ \n  👖            /|",
+        "  😚            😒 \n/👕\         <👗> \n  👖             /|",
+        "  😍         ☺️ \n/👕\      /👗\ \n  👖          /|",
+        "  😍          😍 \n/👕\       /👗\ \n  👖           /|",
+        "  😘   😊 \n /👕\/👗\ \n   👖   /|",
+        " 😳  😁 \n /|\ /👙\ \n /     / |",
+        "😈    /😰\ \n<|\      👙 \n /🍆    / |",
+        "😅 \n/(),✊😮 \n /\         _/\\/|",
+        "😎 \n/\\_,__😫 \n  //    //       \\",
+        "😖 \n/\\_,💦_😋  \n  //         //        \\",
+        "  😭      ☺️ \n  /|\   /(👶)\ \n  /!\   / \ ",
+        "Abee aur kitna dekhoge be besharmi ki bhi hadd hoti hai..,The End 😂...",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 103])
+
+
+@Pbx_cmd(pattern="smoon$")
+async def _(event):
+    event = await eor(event, "smoon")
+    animation_interval = 0.1
+    animation_ttl = range(101)
+    await event.edit("smoon..")
+    animation_chars = [
+        "🌗🌗🌗🌗🌗\n🌓🌓🌓🌓🌓\n🌗🌗🌗🌗🌗\n🌓🌓🌓🌓🌓\n🌗🌗🌗🌗🌗",
+        "🌘🌘🌘🌘🌘\n🌔🌔🌔🌔🌔\n🌘🌘🌘🌘🌘\n🌔🌔🌔🌔🌔\n🌘🌘🌘🌘🌘",
+        "🌑🌑🌑🌑🌑\n🌕🌕🌕🌕🌕\n🌑🌑🌑🌑🌑\n🌕🌕🌕🌕🌕\n🌑🌑🌑🌑🌑",
+        "🌒🌒🌒🌒🌒\n🌖🌖🌖🌖🌖\n🌒🌒🌒🌒🌒\n🌖🌖🌖🌖🌖\n🌒🌒🌒🌒🌒",
+        "🌓🌓🌓🌓🌓\n🌗🌗🌗🌗🌗\n🌓🌓🌓🌓🌓\n🌗🌗🌗🌗🌗\n🌓🌓🌓🌓🌓",
+        "🌔🌔🌔🌔🌔\n🌘🌘🌘🌘🌘\n🌔🌔🌔🌔🌔\n🌘🌘🌘🌘🌘\n🌔🌔🌔🌔🌔",
+        "🌕🌕🌕🌕🌕\n🌑🌑🌑🌑🌑\n🌕🌕🌕🌕🌕\n🌑🌑🌑🌑🌑\n🌕🌕🌕🌕🌕",
+        "🌖🌖🌖🌖🌖\n🌒🌒🌒🌒🌒\n🌖🌖🌖🌖🌖\n🌒🌒🌒🌒🌒\n🌖🌖🌖🌖🌖",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 8])
+
+
+@Pbx_cmd(pattern="tmoon$")
+async def _(event):
+    event = await eor(event, "tmoon")
+    animation_interval = 0.1
+    animation_ttl = range(117)
+    await event.edit("tmoon")
+    animation_chars = [
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+        "🌗",
+        "🌘",
+        "🌑",
+        "🌒",
+        "🌓",
+        "🌔",
+        "🌕",
+        "🌖",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 32])
+
+
+@Pbx_cmd(pattern="hart$")
+async def _(event):
+    animation_interval = 0.5
+    animation_ttl = range(20)
+    event = await eor(event, "❤️")
+    animation_chars = ["🖤", "❤️", "🖤", "❤️", "‎"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 4])
+
+
+@Pbx_cmd(pattern="anim$")
+async def _(event):
+    animation_interval = 1
+    animation_ttl = range(20)
+    event = await eor(event, "😢")
+    animation_chars = [
+        "😁",
+        "😧",
+        "😡",
+        "😢",
+        "😁",
+        "😧",
+        "😡",
+        "😢",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 10])
+
+
+@Pbx_cmd(pattern="fuck$")
+async def _(event):
+    animation_interval = 1
+    animation_ttl = range(0, 101)
+    await eor(event, "fuk")
+    animation_chars = ["👉       ✊️", "👉     ✊️", "👉  ✊️", "👉✊️💦"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 4])
+
+
+@Pbx_cmd(pattern="sux$")
+async def _(event):
+    animation_interval = 1
+    animation_ttl = range(0, 101)
+    await eor(event, "sux")
+    animation_chars = ["🤵       👰", "🤵     👰", "🤵  👰", "🤵👼👰"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 4])
+
+
+@Pbx_cmd(pattern="kiss$")
+async def _(event):
+    animation_interval = 1
+    animation_ttl = range(0, 101)
+    await eor(event, "kiss")
+    animation_chars = ["🤵       👰", "🤵     👰", "🤵  👰", "🤵💋👰"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 4])
+
+
+@Pbx_cmd(pattern="fnl$")
+async def _(event):
+    animation_interval = 2
+    animation_ttl = range(6)
+    event = await eor(event, "Hey There....")
+    animation_chars = ["😁🏿", "😁🏾", "😁🏽", "😁🏼", "‎😁"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 6])
+
+
+@Pbx_cmd(pattern="monkey$")
+async def _(event):
+    animation_interval = 2
+    animation_ttl = range(12)
+    event = await eor(event, "Hey There....")
+    animation_chars = ["🐵", "🙉", "🙈", "🙊", "🖕‎🐵🖕"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 6])
+
+
+@Pbx_cmd(pattern="hand$")
+async def _(event):
+    animation_interval = 1
+    animation_ttl = range(13)
+    event = await eor(event, "🖐️")
+    animation_chars = [
+        "👈",
+        "👉",
+        "☝️",
+        "👆",
+        "🖕",
+        "👇",
+        "✌️",
+        "🤞",
+        "🖖",
+        "🤘",
+        "🤙",
+        "🖐️",
+        "👌",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 13])
+
+
+@Pbx_cmd(pattern="gsg$")
+async def _(event):
+    animation_interval = 1
+    animation_ttl = range(12)
+    event = await eor(event, "ContDown....")
+    animation_chars = [
+        "🔟",
+        "9️⃣",
+        "8️⃣",
+        "7️⃣",
+        "6️⃣",
+        "5️⃣",
+        "4️⃣",
+        "3️⃣",
+        "2️⃣",
+        "1️⃣",
+        "0️⃣",
+        "🆘",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 12])
+
+@Pbx_cmd(pattern="bad$")
+async def _(event):
+    animation_interval = 2
+    animation_ttl = range(12)
+    event = await eor(event, "🦋 𝐌ʏ 𝐂ᴜᴛᴇ 𝐎ᴡɴᴇʀ ❤️")
+    animation_chars = [
+        "❤️",
+        "🖤",
+        "💛",
+        "💘",
+        "💝",
+        "💖",
+        "💗",
+        "💓",
+        "💞",
+        "💜",
+        "❤️‍🔥",
+        "[⎯꯭̽🇨🇦꯭꯭ ⃪В꯭α꯭∂ ꯭м꯭υ꯭η∂꯭α_꯭آآ⎯꯭ ꯭̽🌸](https://t.me/II_BAD_MUNDA_II)",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 12])
+
+@Pbx_cmd(pattern="devil$")
+async def _(event):
+    animation_interval = 2
+    animation_ttl = range(12)
+    event = await eor(event, "🙈 𝐌ʏ 𝐂ᴜᴛᴇ 𝐎ᴡɴᴇʀ ❤️")
+    animation_chars = [
+        "💘",
+        "💖",
+        "💝",
+        "💗",
+        "💜",
+        "🖤",
+        "🤍",
+        "💛",
+        "❤️",
+        "❣️",
+        "💞",
+        "[𐏓 ⃪⃝💸 ꯭𝗖ᴜᴛᴇ᳢᪵•𝗗ᴇᴠɪ𝗟 ⃪آ͢آ🦅⃚⃮⃕⃔⃤ ꯭༎ࠫ⛧‌ٖٖٖٖٖٖٜٖٖٖٖ](https://t.me/ll_mxni_ll)",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 12])
+        
+@Pbx_cmd(pattern="theart$")
+async def _(event):
+    animation_interval = 0.3
+    animation_ttl = range(54)
+    event = await eor(event, "🖤")
+    animation_chars = [
+        "❤️",
+        "🧡",
+        "💛",
+        "💚",
+        "💙",
+        "💜",
+        "🖤",
+        "💘",
+        "💝",
+        "❤️",
+        "🧡",
+        "💛",
+        "💚",
+        "💙",
+        "💜",
+        "🖤",
+        "💘",
+        "💝",
+    ]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 18])
+
+
+CmdHelp("animoji").add_command(
+    "think", None, "Use and see"
+).add_command(
+    "ccry", None, "Use and see"
+).add_command(
+    "fap", None, "Use and see"
+).add_command(
+    "lmao", None, "Use and see"
+).add_command(
+    "nothappy", None, "Use and see"
+).add_command(
+    "clock", None, "Use and see"
+).add_command(
+    "muah", None, "Use and see"
+).add_command(
+    "heart", None, "Use and see"
+).add_command(
+    "gym", None, "Use and see"
+).add_command(
+    "earth", None, "Use and see"
+).add_command(
+    "moon", None, "Use and see"
+).add_command(
+    "lovestory", None, "Turu Lob"
+).add_command(
+    "smoon", None, "Use and see"
+).add_command(
+    "tmoon", None, "Use and see"
+).add_command(
+    "hart", None, "Use and see"
+).add_command(
+    "anim", None, "Use and see"
+).add_command(
+    "fuck", None, "Use and see"
+).add_command(
+    "sux", None, "Use and see"
+).add_command(
+    "kiss", None, "Kya dekh rha h jhopdike."
+).add_command(
+    "fnl", None, "Use and See."
+).add_command(
+    "monkey", None, "Use and see."
+).add_command(
+    "hand", None, "Use and See."
+).add_command(
+    "gsg", None, "Use and See."
+).add_command(
+    "bad", None, "Use and see"
+).add_command(
+    "devil", None, "Use and see"
+).add_command(
+    "theart", None, "Hearts Animation."
+).add()

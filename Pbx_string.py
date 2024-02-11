22d1fdd51bf5c33c6285a4e3395e13a9335dc2a4
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-02-11 09:48:51 +0530

    Update Pbx_string.py

--- Pbx_string.py
+++ Pbx_string.py
@@ -34,17 +34,17 @@ except:
 
 def main():
     print("T E A M    H E L L B O T   ! !")
-    print("Hello!! Welcome to PbxBot Session Generator\n")
+    print("Hello!! Welcome to HellBot Session Generator\n")
     print("Human Verification Required !!")
     while True:
         verify = int(randint(1, 50))
         okvai = int(input(f"Enter {verify} to continue: "))
         if okvai == verify:
-            print("\nChoose the string session type: \n1. PbxBot \n2. Telethon \n3. Pyrogram \n4. Instagram")
+            print("\nChoose the string session type: \n1. HellBot \n2. Telethon \n3. Pyrogram \n4. Instagram")
             while True:
                 library = input("\nYour Choice: ")
                 if library == "1":
-                    generate_Pbxbot_session()
+                    generate_hellbot_session()
                     break
                 elif library == "2":
                     generate_telethon_session()
@@ -62,18 +62,18 @@ def main():
             print("Verification Failed! Try Again:")
 
 
-def generate_Pbxbot_session():
-    print("!!! PBXBOT SESSION !!!")
-    print("One session for all PBXBot's Project.")
+def generate_hellbot_session():
+    print("!!! HELLBOT SESSION !!!")
+    print("One session for all HellBot's Project.")
     api_id = int(input("\nEnter APP ID here: "))
     api_hash = input("\nEnter API_HASH here: ")
-    with PClient(name="Pbxuser", api_id=api_id, api_hash=api_hash, in_memory=True) as Pbx:
-        print("\nYour PBXBOT SESSION is saved in your telegram saved messages.")
-        _session = Pbx.export_session_string()
-        Pbx_session = Pbxbot_session(_session)
-        Pbx.send_message(
+    with PClient(name="helluser", api_id=api_id, api_hash=api_hash, in_memory=True) as hell:
+        print("\nYour HELLBOT SESSION is saved in your telegram saved messages.")
+        _session = hell.export_session_string()
+        hell_session = hellbot_session(_session)
+        hell.send_message(
             "me",
-            f"#PBXBOT_SESSION \n\n`{Pbx_session}`",
+            f"#HELLBOT_SESSION \n\n`{hell_session}`",
         )
 
 
@@ -81,28 +81,28 @@ def generate_pyro_session():
     print("Pyrogram Session for Music Bot!")
     APP_ID = int(input("\nEnter APP ID here: "))
     API_HASH = input("\nEnter API HASH here: ")
-    with PClient(name="Pbxuser", api_id=APP_ID, api_hash=API_HASH, in_memory=True) as Pbxbot:
-        print("\nYour PbxBot Session Is sent in your Telegram Saved Messages.")
-        Pbxbot.send_message(
+    with PClient(name="helluser", api_id=APP_ID, api_hash=API_HASH, in_memory=True) as hellbot:
+        print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
+        hellbot.send_message(
             "me",
-            f"#PBXBOT #PYROGRAM\n\n`{Pbxbot.export_session_string()}`",
+            f"#HELLBOT #PYROGRAM\n\n`{hellbot.export_session_string()}`",
         )
 
 
 def generate_telethon_session():
-    print("\nTelethon Session For PbxBot!")
+    print("\nTelethon Session For HellBot!")
     APP_ID = int(input("\nEnter APP ID here: "))
     API_HASH = input("\nEnter API HASH here: ")
-    with TelegramClient(StringSession(), APP_ID, API_HASH) as Pbxbot:
-        print("\nYour PbxBot Session Is sent in your Telegram Saved Messages.")
-        Pbxbot.send_message(
+    with TelegramClient(StringSession(), APP_ID, API_HASH) as hellbot:
+        print("\nYour HellBot Session Is sent in your Telegram Saved Messages.")
+        hellbot.send_message(
             "me",
-            f"#PBXBOT #TELETHON \n\n`{Pbxbot.session.save()}`",
+            f"#HELLBOT #TELETHON \n\n`{hellbot.session.save()}`",
         )
 
 
 def generate_insta_session():
-    print("Instagram Session For PbxBot!")
+    print("Instagram Session For HellBot!")
     cl = IClient()
     username = input("Enter your Instagram Username: ")
     password = input("Enter your Instagram Password: ")
@@ -126,7 +126,7 @@ def challenge_code(username, choice):
     return otp
 
 
-def Pbxbot(text):
+def hellbot(text):
     res = ''.join(
         map(
             random.choice,
@@ -136,7 +136,7 @@ def Pbxbot(text):
     return res.strip()
 
 
-def Pbxbot_session(session):
+def hellbot_session(session):
     pyro_format = {
         351: ">B?256sI?",
         356: ">B?256sQ?",
@@ -151,7 +151,7 @@ def Pbxbot_session(session):
         5: "91.108.56.130",
     }
 
-    error_msg = "Error in generating session! Report it in Pbx Chats"
+    error_msg = "Error in generating session! Report it in Hell Chats"
 
     # converting pyrogram session
     if len(session) in pyro_format.keys():
@@ -166,7 +166,7 @@ def Pbxbot_session(session):
                 base64.urlsafe_b64decode(session + "=" * (-len(session) % 4)),
             )
 
-        # https://github.com/badmunda98/Telethon/blob/v1/telethon/sessions/string.py
+        # https://github.com/HellBoy-OP/Telethon/blob/v1/telethon/sessions/string.py
         new_session = CURRENT_VERSION + StringSession.encode(
             struct.pack(
                 _STRUCT_PREFORMAT.format(4),
@@ -176,9 +176,10 @@ def Pbxbot_session(session):
                 auth_key
             )
         )
-        return f"=={Pbxbot('Pbx')}{new_session}{Pbxbot('bot')}=="
+        return f"=={hellbot('hell')}{new_session}{hellbot('bot')}=="
     else:
         return error_msg
 
 
 main()
+    

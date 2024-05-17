f7025a35f4a74eebc7d9c23dd185b863da8b9b81
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-05-17 10:58:47 +0530

    Update animation3.py

--- TelethonPbx/plugins/animation3.py
+++ TelethonPbx/plugins/animation3.py
@@ -3,3 +3,12 @@ from collections import deque
 
 from telethon.tl.functions.users import GetFullUserRequest
 from TelethonPbx.plugins import *
+
+
+@Pbx_cmd(pattern="tank$")
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    await typew.edit("█۞███████]▄▄▄▄▄▄▄▄▄▄▃ \n"
+                     "▂▄▅█████████▅▄▃▂…\n"
+                     "[███████████████████]\n"
+                     "◥⊙▲⊙▲⊙▲⊙▲⊙▲⊙▲⊙◤\n")

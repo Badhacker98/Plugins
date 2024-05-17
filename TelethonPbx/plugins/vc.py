8bd0bdefe5f02ebeb2cf639f9fc57e00aff0d72c
ʙᴀᴅ ʙᴀʙʏ <154504921+Badhacker98@users.noreply.github.com>
2024-05-17 09:04:01 +0530

    Update vc.py

--- TelethonPbx/plugins/vc.py
+++ TelethonPbx/plugins/vc.py
@@ -1,6 +1,5 @@
 import asyncio
 
-from pytgcalls.exceptions import NotConnectedError
 
 from telethon.tl.functions.channels import GetFullChannelRequest as getchat
 from telethon.tl.functions.phone import CreateGroupCallRequest as startvc
